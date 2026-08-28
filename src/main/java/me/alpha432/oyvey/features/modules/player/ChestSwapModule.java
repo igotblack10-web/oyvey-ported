a7ff8af2856d14e6b8a3285e99a70e0fb3059507
igotblack10-web <igotblack10@gmail.com>
2026-08-28 17:14:13 +0300

    Add ChestSwap player module

---
+++ src/main/java/me/alpha432/oyvey/features/modules/player/ChestSwapModule.java
@@ -0,0 +1,9 @@
+package me.alpha432.oyvey.features.modules.player;
+
+import me.alpha432.oyvey.features.modules.Module;
+
+public class ChestSwapModule extends Module {
+    public ChestSwapModule() {
+        super("ChestSwap", "Swaps between chest armor and an elytra.", Category.PLAYER);
+    }
+}
