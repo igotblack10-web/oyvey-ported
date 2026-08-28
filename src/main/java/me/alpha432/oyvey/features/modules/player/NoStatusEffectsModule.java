2a357410cdfaabe1e0b9258689ce646fb253eee1
igotblack10-web <igotblack10@gmail.com>
2026-08-28 17:06:24 +0300

    Add NoStatusEffects player module

---
+++ src/main/java/me/alpha432/oyvey/features/modules/player/NoStatusEffectsModule.java
@@ -0,0 +1,10 @@
+package me.alpha432.oyvey.features.modules.player;
+
+import me.alpha432.oyvey.features.modules.Module;
+
+/** Placeholder for disabling unwanted status-effect visuals/effects. */
+public class NoStatusEffectsModule extends Module {
+    public NoStatusEffectsModule() {
+        super("NoStatusEffects", "Disables status effects.", Category.PLAYER);
+    }
+}
