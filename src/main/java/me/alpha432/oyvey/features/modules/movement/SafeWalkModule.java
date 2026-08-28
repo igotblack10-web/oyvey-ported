d27e78ae021c0f4c595c79a44b3799a3d3a91856
igotblack10-web <igotblack10@gmail.com>
2026-08-28 16:57:54 +0300

    Clean up SafeWalk module implementation

--- src/main/java/me/alpha432/oyvey/features/modules/movement/SafeWalkModule.java
+++ src/main/java/me/alpha432/oyvey/features/modules/movement/SafeWalkModule.java
@@ -2,14 +2,12 @@ package me.alpha432.oyvey.features.modules.movement;
 
 import me.alpha432.oyvey.features.modules.Module;
 
+/**
+ * Prevents the local player from stepping off ledges while enabled.
+ * The actual movement hook lives in MixinSafeWalk.
+ */
 public class SafeWalkModule extends Module {
     public SafeWalkModule() {
         super("SafeWalk", "Prevents walking off block edges.", Category.MOVEMENT);
-    }
-
-    @Override
-    public void onTick() {
-        if (nullCheck()) return;
-        if (mc.player.onGround()) mc.player.setOnGround(true);
     }
 }
