8325484f2fe7149ab486ae48e94171b429de1cdf
igotblack10-web <igotblack10@gmail.com>
2026-08-28 17:06:05 +0300

    Add AutoEat player module

---
+++ src/main/java/me/alpha432/oyvey/features/modules/player/AutoEatModule.java
@@ -0,0 +1,26 @@
+package me.alpha432.oyvey.features.modules.player;
+
+import me.alpha432.oyvey.features.modules.Module;
+import me.alpha432.oyvey.features.settings.Setting;
+
+public class AutoEatModule extends Module {
+    private final Setting<Integer> health = num("Health", 8, 1, 20);
+
+    public AutoEatModule() {
+        super("AutoEat", "Automatically eats food when health is low.", Category.PLAYER);
+    }
+
+    @Override
+    public void onTick() {
+        if (nullCheck()) return;
+        if (mc.player.getHealth() > health.getValue()) return;
+        if (mc.player.getUseItem().isEmpty() && mc.player.getInventory().items.stream().anyMatch(stack -> stack.isEdible())) {
+            mc.options.keyUse.setDown(true);
+        }
+    }
+
+    @Override
+    public void onDisable() {
+        if (mc.options != null) mc.options.keyUse.setDown(false);
+    }
+}
