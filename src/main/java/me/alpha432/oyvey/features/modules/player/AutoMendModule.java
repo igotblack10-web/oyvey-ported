818f12c90f4f7e799477586b2a71a63a6c6be158
igotblack10-web <igotblack10@gmail.com>
2026-08-28 17:13:05 +0300

    Add AutoMend player module

---
+++ src/main/java/me/alpha432/oyvey/features/modules/player/AutoMendModule.java
@@ -0,0 +1,9 @@
+package me.alpha432.oyvey.features.modules.player;
+
+import me.alpha432.oyvey.features.modules.Module;
+
+public class AutoMendModule extends Module {
+    public AutoMendModule() {
+        super("AutoMend", "Helps maintain damaged mending equipment.", Category.PLAYER);
+    }
+}
