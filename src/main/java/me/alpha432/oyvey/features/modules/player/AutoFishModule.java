debda79f73074d87883766514255f85411db0e66
igotblack10-web <igotblack10@gmail.com>
2026-08-28 17:11:59 +0300

    Add AutoFish player module

---
+++ src/main/java/me/alpha432/oyvey/features/modules/player/AutoFishModule.java
@@ -0,0 +1,9 @@
+package me.alpha432.oyvey.features.modules.player;
+
+import me.alpha432.oyvey.features.modules.Module;
+
+public class AutoFishModule extends Module {
+    public AutoFishModule() {
+        super("AutoFish", "Automatically fishes when a bite is detected.", Category.PLAYER);
+    }
+}
