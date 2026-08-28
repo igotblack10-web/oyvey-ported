ce8ec1eaef781d2b9454c3b74a72be8663155614
igotblack10-web <igotblack10@gmail.com>
2026-08-28 17:11:26 +0300

    Add AutoReplenish player module

---
+++ src/main/java/me/alpha432/oyvey/features/modules/player/AutoReplenishModule.java
@@ -0,0 +1,9 @@
+package me.alpha432.oyvey.features.modules.player;
+
+import me.alpha432.oyvey.features.modules.Module;
+
+public class AutoReplenishModule extends Module {
+    public AutoReplenishModule() {
+        super("AutoReplenish", "Refills low-count hotbar stacks from the inventory.", Category.PLAYER);
+    }
+}
