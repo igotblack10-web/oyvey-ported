370787b1fb1713f157ebbce3a7387507cffd91f6
igotblack10-web <igotblack10@gmail.com>
2026-08-28 17:19:36 +0300

    Add NoPush player module

---
+++ src/main/java/me/alpha432/oyvey/features/modules/player/NoPushModule.java
@@ -0,0 +1,9 @@
+package me.alpha432.oyvey.features.modules.player;
+
+import me.alpha432.oyvey.features.modules.Module;
+
+public class NoPushModule extends Module {
+    public NoPushModule() {
+        super("NoPush", "Prevents entities from pushing the player.", Category.PLAYER);
+    }
+}
