ee97f8962b83e68f4d3ca6572eeb9bbcd245d67e
igotblack10-web <igotblack10@gmail.com>
2026-08-28 17:15:29 +0300

    Add NoFall movement module

---
+++ src/main/java/me/alpha432/oyvey/features/modules/movement/NoFallModule.java
@@ -0,0 +1,9 @@
+package me.alpha432.oyvey.features.modules.movement;
+
+import me.alpha432.oyvey.features.modules.Module;
+
+public class NoFallModule extends Module {
+    public NoFallModule() {
+        super("NoFall", "Prevents fall damage.", Category.MOVEMENT);
+    }
+}
