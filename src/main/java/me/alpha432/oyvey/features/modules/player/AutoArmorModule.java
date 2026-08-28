3483eeb887bca04b09a42d728664e3c83c016b64
igotblack10-web <igotblack10@gmail.com>
2026-08-28 17:13:27 +0300

    Add AutoArmor player module

---
+++ src/main/java/me/alpha432/oyvey/features/modules/player/AutoArmorModule.java
@@ -0,0 +1,9 @@
+package me.alpha432.oyvey.features.modules.player;
+
+import me.alpha432.oyvey.features.modules.Module;
+
+public class AutoArmorModule extends Module {
+    public AutoArmorModule() {
+        super("AutoArmor", "Automatically equips better armor from the inventory.", Category.PLAYER);
+    }
+}
