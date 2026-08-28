425487b39f0df2f8ed74a7c51fdaabde0ccf76ab
igotblack10-web <igotblack10@gmail.com>
2026-08-28 17:04:30 +0300

    Add AutoTool player module placeholder

---
+++ src/main/java/me/alpha432/oyvey/features/modules/player/AutoToolModule.java
@@ -0,0 +1,9 @@
+package me.alpha432.oyvey.features.modules.player;
+
+import me.alpha432.oyvey.features.modules.Module;
+
+public class AutoToolModule extends Module {
+    public AutoToolModule() {
+        super("AutoTool", "Automatically selects a suitable hotbar tool while mining.", Category.PLAYER);
+    }
+}
