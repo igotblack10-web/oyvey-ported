2dfe785e46e2493a541fbf3c8e3ef7c7bf044b34
igotblack10-web <igotblack10@gmail.com>
2026-08-28 17:10:19 +0300

    Add NoRender render module

---
+++ src/main/java/me/alpha432/oyvey/features/modules/render/NoRenderModule.java
@@ -0,0 +1,9 @@
+package me.alpha432.oyvey.features.modules.render;
+
+import me.alpha432.oyvey.features.modules.Module;
+
+public class NoRenderModule extends Module {
+    public NoRenderModule() {
+        super("NoRender", "Disables selected visual effects.", Category.RENDER);
+    }
+}
